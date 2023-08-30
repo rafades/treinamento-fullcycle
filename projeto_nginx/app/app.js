656eb2259c2324d3a465df59d7966ff8ad4a3c9b
Rafael Francisco Espinola Desiderio <rafades@yahoo.com.br>
2023-08-30 13:05:28 -0300

    fix: recupera informações do banco de dados para a tela

--- projeto_nginx/app/app.js
+++ projeto_nginx/app/app.js
@@ -1,12 +1,61 @@
-const express = require('express');
-const app = express();
 const PORT = 8000;
-const HOST = '0.0.0.0'
+const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST, () => {
-    console.log('listen http://${HOST}:${PORT}');
-})
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: 'views/layouts'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const mysql = require('mysql2');
+
+const connection = mysql.createPool({
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'ngnix_usuario',
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+});
 
 app.get('/', (req, res) => {
-    res.send('FullCycle Rocks!');
+    const nome = req.query.nome;
+    if (nome) {
+        console.log(nome);
+        connection.query("INSERT INTO usuario(nome) values(?)", nome, function (err) {
+            if (err) {
+                throw err;
+            }
+            carregarResponse(res);
+        });
+    } else {
+        carregarResponse(res);
+    }
+})
+
+function carregarResponse(res) {
+    connection.query('SELECT `id`, `nome` FROM `usuario`', function (err, result) {
+        if (err) {
+            throw err;
+        } else {            
+            res.render('index', {
+                layout: 'template',
+                logo: 'FullCycle Rocks!',
+                dados: result
+            });
+        }
+    });
+}
+
+app.listen(PORT, HOST, () => {
+    console.log('listen http://%s:%s', HOST, PORT);
 })
